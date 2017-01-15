08a89fbcc0aa2ad8e5647ebd56c4f896b32f4eb6
a-shamasneh <a.ashmasneh@gmail.com>
2017-01-15 09:00:48 +0200

    toy problem day 2 week 3

--- flatten/flatten.js
+++ flatten/flatten.js
@@ -13,5 +13,26 @@ flatten('a', ['b', 2], 3, null, [[4], ['c']]) // returns ['a', 'b', 2, 3, null, 
 */
 
 function flatten(){
-
+var args=Array.from(arguments);
+var result=[];
+for(var i=0;i<args.length;i++){
+	if(typeof(args[i])=="object"){
+	if(Array.isArray(args[i])){
+		for(var j=0;j<args[i].length;j++){
+			if(typeof(args[i][j])=="object"){
+				for(var k=0;k<args[i][j].length;k++){
+					result.push(args[i][j][k])
+					}
+				}else{result.push(args[i][j])}
+			}
+		}
+	}
+	else{
+		result.push(args[i])
+	}
+	if(JSON.stringify(args[i])==="null"){
+		result.push(null)
+	}
+}
+return result;
 }
