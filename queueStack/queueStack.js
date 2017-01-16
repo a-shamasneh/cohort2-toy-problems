ec70930c19d6672c8d14011c551e5eb3e1227d89
a-shamasneh <a.ashmasneh@gmail.com>
2017-01-16 09:23:23 +0200

    toy d3 w 3

--- queueStack/queueStack.js
+++ queueStack/queueStack.js
@@ -8,17 +8,22 @@
    */
  var Stack = function() {
   this.stack=[];
+  this.counter=0;
 
    // add an item to the top of the stack
    this.push = function(value){
+   	this.stack.push(value);
+   	
    };
 
    // remove an item from the top of the stack
    this.pop = function(){
+   	this.stack.splice(0,1);
    };
 
    // return the number of items in the stack
    this.size = function(){
+   	return this.stack.length;
    };
  };
 
@@ -33,15 +38,22 @@
    // called to add an item to the `queue`
    this.enqueue = function(value){
      
+     inbox.push(value);
      }
-   };
+  
 
    // called to remove an item from the `queue`
    this.dequeue = function(){
-    }
-   };
+   	if(outbox.length==0){
+   while(inbox.length>-1){
+   	outbox.push(inbox.pop())
+   }
+   return outbox.pop();
+    }}
+   
 
    // should return the number of items in the queue
    this.size = function(){
+   	return inbox.length;
    };
  };
