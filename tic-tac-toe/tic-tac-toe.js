89d3a4cb26686dcc0b52946637f04ed86f8cbf2b
a-shamasneh <a.ashmasneh@gmail.com>
2017-01-22 09:25:41 +0200

    tic tac

--- tic-tac-toe/tic-tac-toe.js
+++ tic-tac-toe/tic-tac-toe.js
@@ -22,5 +22,71 @@ ticTacToe(solvedBoard);//should return 1
 */
 
 function ticTacToe(board){
-	
+	var ok=0;
+	var r1=board[0][0];
+	var r12=board[0][1];
+	var r13=board[0][2];
+	var r21=board[1][0];
+	var r22=board[1][1];
+	var r23=board[1][2];
+	var r31=board[2][0];
+	var r32=board[2][1];
+	var r33=board[2][2];
+
+	if(r1==1&&r12==1&&r13==1){
+		return 1;
+
+	}
+	///
+	if(r1==2&&r12==2&&r13==2){
+		return 2;
+	}
+	///
+	if(r1==1&&r21==1&&r31==1){
+		return 1;
+	}
+	///
+	if(r1==2&&r21==2&&r31==2){
+		return 2;
+	}
+	///
+	if(r12==1&&r22==1&&r32==1){
+		return 1;
+	}
+	///
+	if(r13==1&&r23==1&&r33==1){
+		return 1;
+	}
+	//
+	if(r12==2&&r22==2&&r32==2){
+		return 2;
+	}
+	///
+	if(r13==2&&r23==2&&r33==2){
+		return 2;
+	}
+	//
+	if(r1==1&&r22==1&&r33==1){
+		return 1;
+	}
+	///
+	if(r1==2&&r22==2&&r33==2){
+		return 2;
+	}
+	///
+	if(r33==1&&r22==1&&r1==1){
+		return 1;
+	}
+	if(r33==2&&r22==2&&r1==2){
+		return 2;
+	}
+
+	if(ok==0){
+		return -1;
+	}
+
 };
+
+
+
+
